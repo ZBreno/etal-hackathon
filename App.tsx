07883caf9966bb2bfeb1757f56976db201d1fa22
Felippe <felipperian365@gmail.com>
2023-11-30 20:02:53 -0300

    Adicionado lib de icons e styled components

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from "styled-components";
+import Promotions from "./src/screens/Promotions";
 
 export default function App() {
+  const theme = {
+    primary: "#CE2C34",
+    "primary-transparent": "#CE2C3410",
+    background: "#F3F4F6",
+    black: "#353535",
+    gray: "#808388",
+    white: "#ffffff",
+    lightgray: "#E6E6E6",
+    "disabled-color": "#D0D0D0",
+    warning: "#FFE660",
+    error: "#CE2C34",
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <Promotions />
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Promotions/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+// import { Container } from './styles';
+
+const Promotions = () => {
+  return (
+    <View>
+      <Text>Promotions</Text>
+    </View>
+  );
+};
+
+export default Promotions;
