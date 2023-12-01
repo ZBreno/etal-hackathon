61ebf9d683f8ed38cb5628a20912974b5c55dcdd
ZBreno <brenosoares9533@gmail.com>
2023-11-30 21:46:02 -0300

    add: half promotion component

--- src/components/Promotions/styles.ts
+++ src/components/Promotions/styles.ts
@@ -1,46 +1,71 @@
 import styled from "styled-components/native";
 
 export const ViewContainer = styled.View`
-	background: ${(props) => props.theme.white};
-	color: ${(props) => props.theme.white};
-	padding: 16px;
+	background-color: ${(props) => props.theme.colors.white};
+	color: ${(props) => props.theme.colors.white};
 	border-radius: 12px;
-	border: 1px solid ${(props) => props.theme.border};
+	border: 1px solid ${(props) => props.theme.colors.border};
 `;
 
 export const ViewContainerProduct = styled.View`
 	flex-direction: row;
 	gap: 16px;
+	padding: 16px 16px 0px 16px;
 `;
-
 export const TextStore = styled.Text`
-	color: ${(props) => props.theme["text-gray"]};
+	color: ${(props) => props.theme.colors["text-gray"]};
 	font-size: 12px;
-	font-style: normal;
-	font-weight: 500;
-	margin-left: 12px;
+	font-family: ${(props) => props.theme.fonts.medium};
+	margin-left: 10px;
+	align-items: center;
 `;
 export const TextTime = styled.Text`
-	color: ${(props) => props.theme["text-gray"]};
+	color: ${(props) => props.theme.colors["text-gray"]};
 	font-size: 12px;
-	font-style: normal;
-	font-weight: 500;
+	font-family: ${(props) => props.theme.fonts.medium};
+
 `;
 
 export const TitleProduct = styled.Text`
-	color: ${(props) => props.theme.black};
+	color: ${(props) => props.theme.colors.black};
 	font-size: 16px;
-	flex-wrap: wrap;
+	font-family: ${(props) => props.theme.fonts.medium};
+	margin-top: 8px;
+
 `;
 
 export const TextDelivery = styled.Text`
-	color: ${(props) => props.theme.green};
+	color: ${(props) => props.theme.colors.green};
 	font-size: 14px;
-	font-weight: 500;
-	background-color:  ${(props) => props.theme.lightgreen};
-	padding: 8px 12px;
+	font-family: ${(props) => props.theme.fonts.medium};
+	background-color: ${(props) => props.theme.colors.lightgreen};
+	padding: 4px 12px;
+	text-align: center;
 	border-radius: 8px;
 	margin-top: 8px;
 	align-items: flex-start;
 	justify-content: flex-start;
+`;
+
+export const TextOldPrice = styled.Text`
+	color: ${(props) => props.theme.colors.gray};
+	text-decoration: line-through;
+	font-size: 12px;
+	font-family: ${(props) => props.theme.fonts.medium};
+	
+`;
+export const TextNewPrice = styled.Text`
+	color: ${(props) => props.theme.colors.primary};
+	font-size: 14px;
+	font-family: ${(props) => props.theme.fonts.bold};
+`;
+
+export const ViewContainerComment = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 16px;
+  padding: 16px;
+	align-items: center;
+  border-top-width: 1px;
+  border-top-color: ${(props) => props.theme.colors.border};
 `;
